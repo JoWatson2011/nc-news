e707ad2f215430e33ed0eee38669f8c1315cce81
jowatson2011 <jowatson@protonmail.com>
2024-05-28 15:06:52 +0100

    Added tests for /api/articles/:id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -60,3 +60,42 @@ describe("GET /api", () => {
       });
   });
 });
+
+describe("GET /api/articles", () => {
+  test("200: Responds with an article object of the queried id", () => {
+    const expectedArticle = {
+      article_id: 1,
+      title: "Living in the shadow of a great man",
+      topic: "mitch",
+      author: "butter_bridge",
+      body: "I find this existence challenging",
+      created_at: "2020-07-09T20:11:00.000Z",
+      votes: 100,
+      article_img_url:
+        "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+    };
+
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then((res) => {
+        expect(res.body.article).toEqual(expectedArticle);
+      });
+  });
+  test("404: Responds with Not Found when passed an id that isn't in the articles table", () => {
+    return request(app)
+      .get("/api/articles/300")
+      .expect(404)
+      .then((res) => {
+        expect(res.body.msg).toBe("Not Found");
+      });
+  });
+  test("400: Responds with Bad Request when passed an id that is not a number", () => {
+    return request(app)
+      .get("/api/articles/notANumber")
+      .expect(400)
+      .then((res) => {
+        expect(res.body.msg).toBe("Bad Request");
+      });
+  });
+});
