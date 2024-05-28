215b38b536af9e02a085d56610cef15273e6beb0
jowatson2011 <jowatson@protonmail.com>
2024-05-28 14:29:17 +0100

    Removed leftover debug message

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -3,7 +3,6 @@ const { fetchTopics } = require("../models/topics");
 exports.getTopics = (req, res, next) => {
 fetchTopics()
   .then((topicData) => {
-    console.log("then controller")
     res.status(200).send({ topics: topicData });
   });
 };
