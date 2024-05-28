ca7aff61a1767fa33d5b9ef713a63b289d176e2c
jowatson2011 <jowatson@protonmail.com>
2024-05-28 15:24:05 +0100

    Removed api model, instead json file required directly into controller

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,6 +1,3 @@
-const { fetchEndpoints } = require("../models/api");
 exports.getEndpoints = (req, res, next) => {
-  fetchEndpoints().then((endpointsData) => {
-    res.status(200).send({ endpoints: endpointsData });
-  });
+  res.status(200).send({ endpoints: require("../endpoints.json") });
 };
--- models/api.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs/promises");
-exports.fetchEndpoints = () => {
-  return fs.readFile("./endpoints.json", "utf8").then((file) => {
-    const parsedFile = JSON.parse(file);
-    return parsedFile;
-  });
-};
