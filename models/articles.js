44afdde462feff15f0213dbccfc09f5e6220acd0
jowatson2011 <jowatson@protonmail.com>
2024-05-29 16:40:24 +0100

    Remove comments

--- models/articles.js
+++ models/articles.js
@@ -35,14 +35,6 @@ exports.fetchArticles = (topic) => {
     .query(
       sqlQuery,
       sqlParams
-      // `SELECT articles.author, articles.title, articles.article_id, articles.topic,
-      //  articles.created_at, articles.votes, articles.article_img_url, 
-      //  COUNT(comments.comment_id) AS comment_count FROM comments
-      // RIGHT JOIN articles 
-      // ON articles.article_id = comments.article_id
-      // GROUP BY articles.author, articles.title, articles.article_id, articles.topic,
-      //  articles.created_at, articles.votes,articles.article_img_url
-      // ORDER BY created_at DESC;`
     )
     .then(({ rows }) => {
       return rows.map((row) => {
